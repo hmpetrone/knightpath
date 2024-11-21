fecfaf1751bc41563dd70133422b589a03195aab
hmpetrone <hernan_hmp@hotmail.com>
2024-11-21 15:54:30 -0300

    add roll action to script

--- scripts/knight.gd
+++ scripts/knight.gd
@@ -20,6 +20,8 @@ var alive = true
 var taking_damage = false
 var died: bool = false
 var praying: bool = false
+var falling: bool = false
+var rolling: bool = false
 
 #player stats
 var damage = 15
@@ -40,36 +42,46 @@ var crouch_cshape = preload("res://resources/knight/collision shapes/knight_crou
 @onready var sounds: Node2D = $sounds
 
 func _physics_process(delta: float) -> void:
-	jump(delta)
 	if died: return
 	move_x()
 	if praying:
 		knight_animations.play("pray")
 		return
+	jump(delta)
 	flip()
 	crouch()
 	first_attack()
+	roll()
 	update_animations()
 	move_and_slide()
 	
 func update_animations():
+	#print(attacking)
 	if not died:
-		if velocity.x and not attacking and not taking_damage:
+		if velocity.x and not attacking and not taking_damage and not falling and not rolling:
 			if is_crouching:
 				knight_animations.play("crouch")
 			else:
 				knight_animations.play("run")
-		elif not attacking and not taking_damage:
+		elif not attacking and not taking_damage and not falling and not rolling:
 			if is_crouching:
 				knight_animations.play("crouch_stay")
 			elif not attacking:
 				knight_animations.play("idle")
-		if not is_on_floor() and not attacking and not taking_damage:
+		if not is_on_floor() and not attacking and not taking_damage and not rolling:
 			if velocity.y < 0:
 				knight_animations.play("jump")
-			else:
+			elif falling:
 				knight_animations.play("fall")
+		elif is_on_floor():
+			falling = false
 
+	if knight_animations.animation != "hurt":
+		taking_damage = false
+	if knight_animations.animation != "roll":
+		rolling = false
+		
+		
 
 func move_x():
 	if died:
@@ -91,11 +103,13 @@ func flip():
 func jump(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not rolling:
 		velocity.y = JUMP_VELOCITY
+	if velocity.y > 0:
+		falling= true
 		
 func crouch():
-	if Input.is_action_just_pressed("down"):
+	if Input.is_action_just_pressed("down") and not rolling:
 		is_crouching = true
 		idle_shape.position.y = -12
 		hitbox_cs.position.y = -12
@@ -106,8 +120,8 @@ func crouch():
 	if Input.is_action_just_released("down"):
 		is_crouching = false
 		idle_shape.position.y = -18
-		idle_shape.shape = standing_cshape
 		hitbox_cs.position.y = -18
+		idle_shape.shape = standing_cshape
 		hitbox_cs.shape = standing_cshape
 		set_movement_speed('default')
 
@@ -117,9 +131,11 @@ func set_movement_speed(state: String):
 			speed = SPEED
 		"crouching":
 			speed = SPEED * 0.3
+		"rolling":
+			speed = SPEED * 2
 
 func first_attack():
-	if Input.is_action_just_pressed("first_attack"):
+	if Input.is_action_just_pressed("first_attack") and not rolling:
 		attacking = true
 		speed = speed * 0.2
 		if is_crouching:
@@ -130,6 +146,15 @@ func first_attack():
 			knight_animations.play("first_attack")
 		prop.disabled = false
 
+func roll():
+	if Input.is_action_just_pressed("roll") and velocity.x != 0 and is_on_floor() and not attacking:
+		rolling = true
+		idle_shape.position.y = -12
+		hitbox_cs.position.y = -12
+		idle_shape.shape = crouch_cshape
+		hitbox_cs.shape = crouch_cshape
+		knight_animations.play("roll")
+		set_movement_speed('rolling')
 		
 func death():
 	health = min_health
@@ -143,16 +168,19 @@ func take_damage(damage_received):
 	if health <= min_health:
 		death()
 	else:
+		rolling = false
+		idle_shape.position.y = -18
+		hitbox_cs.position.y = -18
+		idle_shape.shape = standing_cshape
+		hitbox_cs.shape = standing_cshape
+		set_movement_speed('default')
 		taking_damage = true
 		knight_animations.play("hurt")
 		sounds.get_child(randi_range(0,2)).play()
 		await get_tree().create_timer(0.2).timeout
-		if knight_animations.animation != "hurt":
-			taking_damage = false
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if not died: take_damage(area.damage)
-
 
 func _on_knight_animations_animation_finished() -> void:
 	if knight_animations.animation == "first_attack" or knight_animations.animation == "crouching_first_attack":
@@ -162,3 +190,12 @@ func _on_knight_animations_animation_finished() -> void:
 		speed = SPEED
 	if knight_animations.animation == "hurt":
 		taking_damage = false
+	if knight_animations.animation == "fall":
+		knight_animations.frame = 1
+	if knight_animations.animation == "roll":
+		rolling = false
+		idle_shape.position.y = -18
+		hitbox_cs.position.y = -18
+		idle_shape.shape = standing_cshape
+		hitbox_cs.shape = standing_cshape
+		set_movement_speed('default')
